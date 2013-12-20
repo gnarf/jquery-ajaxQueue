fcd5771eaf2206e4a7c0f53208b9d1b4bf2bb8ef
caoglish <caoglish.email+helperbar@gmail.com>
2013-12-20 23:42:05 +1100

    1. add mockjax to do ajax testing 2. update unit test script

--- test/jquery.ajaxQueue_test.js
+++ test/jquery.ajaxQueue_test.js
@@ -1,5 +1,5 @@
 /*global sinon, window*/
-(function( $ ) {
+(function($) {
 	/*
 		======== A Handy Little QUnit Reference ========
 		http://docs.jquery.com/QUnit
@@ -19,17 +19,59 @@
 			raises(block, [expected], [message])
 	*/
 
-module( "ajaxQueue", {
-	setup: function() {
-	},
-	teardown: function() {
-	}
-});
+	var resultCache;
+	module("ajaxQueue", {
+		setup: function() {
+			resultCache = [];
+			for(var i=0; i<10;i++){
+				$.mockjax({
+					url: '/test/inline'+i,
+					dataType: 'json',
+					responseTime: Math.random()*150+1,
+					responseText: {
+						data: i
+					}
+				});
+			}
+		},
+		teardown: function() {}
+	});
 
-test( "OK", function() {
-	expect( 1 );
-	ok( true );
-});
+	asyncTest("$.ajaxQueue request only one, work like single ajax", function() {
+		$.ajaxQueue({
+			url: '/test/inline1',
+			dataType: 'json'
+		}).done(function(result) {
+			console.log(result);
+			equal(result.data, 1, 'response data is 1');
+			start();
+		});
+
+	});
+
+	asyncTest("$.ajaxQueue request in pipe", function() {
+		var url;
+		for (var i=0; i < 10; i++) {
+			url= '/test/inline'+i;
+			$.ajaxQueue({
+				url: url,
+				dataType: 'json'
+			}).done(function(result) {
+				resultCache.push(result.data);
+				console.log(result.data);
+				
+			});
+		}
+
+		$.ajaxQueue({
+			url: '/test/inline1',
+			dataType: 'json'
+		}).done(function(result) {
+			deepEqual(resultCache,[0,1,2,3,4,5,6,7,8,9]);
+			start();
+		});
+
+	});
 
 
 }(jQuery));
